b90b1cf63bc67ed3d0c9dce726d230fae32300bd
Sarah <schretters@proton.me>
2025-07-26 21:47:04 -0500

    Fix cleaning logic

--- prime_scraper.py
+++ prime_scraper.py
@@ -59,7 +59,7 @@ def safe_history(url):
 
 frames = []
 for url_name in prime_sets:
-    frames.append(get_history(url_name))
+    frames.append(safe_history(url_name))
     time.sleep(1)  # to avoid hitting API rate limits
 
 raw = pd.concat(frames, ignore_index=True)
@@ -80,11 +80,11 @@ clean = (
 )
 
 # remove duplicates in case of API errors
-raw.sort_values(["item", "datetime"], inplace=True)
-clean = raw.drop_duplicates(subset=["item", "datetime"]) 
+clean.sort_values(["item", "datetime"], inplace=True)
+clean = clean.drop_duplicates(subset=["item", "datetime"])
 
 # remove rows with no volume data
-raw = raw.dropna(subset=["volume"])
+clean = clean.dropna(subset=["volume"])
 
 
 # split datetime for easier grouping
